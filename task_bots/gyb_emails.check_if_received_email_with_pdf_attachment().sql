7e83908530f31760ca4b919ccc81cdc516727a8f
Alexey <aignatyuk@mail.ru>
2017-11-24 18:05:55 +0500

    added check_if_received_email_with_pdf_attachment trigger

--- task_bots/gyb_emails.check_if_received_email_with_pdf_attachment().sql
+++ task_bots/gyb_emails.check_if_received_email_with_pdf_attachment().sql
@@ -1,23 +1,23 @@
-create or replace function gyb_emails.check_if_csv_file_arrived()
+create or replace function gyb_emails.check_if_received_email_with_pdf_attachment()
 	returns trigger
  	LANGUAGE plpgsql
 AS $function$
 declare
-	_file_path text;
+_file_path text;
 	_message_uid text;
 	_customer_id int;
 	_js text[];
-	_f_csv boolean;
+	_f_pdf boolean;
 	_mail_exist boolean;
 begin	
-	raise notice 'START check_if_csv_file_arrived';
+	raise notice 'START check_if_received_email_with_pdf_attachment';
 	select new.attachment_path , new.message_uid into _file_path, _message_uid;
 	_file_path = gyb_emails.get_s3_file_path(_file_path);
 	raise notice 'file_path = %',_file_path;
-	_f_csv = _file_path like '%.csv';
-	raise notice 'file_path_csv = %',_f_csv;
-	if  _f_csv = 't' then
-		raise notice 'file_path_csv ';
+	_f_pdf = _file_path like '%.pdf';
+	raise notice 'file_path_pdf = %',_f_pdf;
+	if  _f_pdf = true then
+		raise notice 'file_path_pdf ';
 		select count(customer_id)>0 from gyb_emails.messages into _mail_exist where message_uid = _message_uid and email_to = 'bank@revisor1.dk';
 		if _mail_exist = true then
 			select customer_id from gyb_emails.messages into _customer_id where message_uid = _message_uid and email_to = 'bank@revisor1.dk';
@@ -27,20 +27,17 @@ begin
 			raise notice 'customer_id = %',_customer_id;
 			_js = array[cast(_customer_id as text),_file_path];
 			raise notice 'json = %',_js;
-			perform task_bots.create_task ('import_csv_file', _js);
-			new.attachment_printed = 'passed to import_csv_file task';
+			perform task_bots.create_task ('bank@_received_email_with_pdf_attachment', _js);
+			new.attachment_printed = 'passed to bank@_received_email_with_pdf_attachment task';
 		end if;	
 	end if;
-	raise notice 'END check_if_csv_file_arrived';
+	raise notice 'END check_if_received_email_with_pdf_attachment';
 	return new;
-end;
+	end;
 $function$;
 
-
-create trigger check_if_csv_file_arrived BEFORE INSERT OR UPDATE
+create trigger check_if_received_email_with_pdf_attachment BEFORE INSERT OR UPDATE
         on gyb_emails.attachments
          for each row
-         execute procedure check_if_csv_file_arrived();         
+         execute procedure check_if_received_email_with_pdf_attachment();
          
-         
-

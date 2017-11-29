bdc173a14d1a8cb30a28338e1ac17d72681c0d36
Alexey <aignatyuk@mail.ru>
2017-11-29 14:19:52 +0500

    choose_startup_email_to_send_bot and choose_startup_email_to_send task type creation

---
+++ task_bots/choose_startup_email_to_send_bot and choose_startup_email_to_send task type.sql
@@ -0,0 +1,41 @@
+-------------------------------------------------------------------------------------------------------------------------------------------
+-- PREPARATIONS
+-------------------------------------------------------------------------------------------------------------------------------------------
+
+insert into task_manager.freelancers(worker_initials) values ('choose_startup_email_to_send_bot');
+insert into task_bots.bots(name , type,path) values ('choose_startup_email_to_send_bot',1,'task_bots.choose_startup_email_to_send_bot');
+
+insert into task_manager.column_task_description (column_name ,default_freelancer,category,is_standalone,problem_description )
+												 values('choose_startup_email_to_send','choose_startup_email_to_send_bot', 'bank',true,'');
+
+--insert into  task_manager.task_parameters (task_type_id,parameter_name,data_type) values(1008, 'customer_id','int');
+--insert into  task_manager.task_parameters (task_type_id,parameter_name,data_type) values(1008, 'message_uid','text');
+
+CREATE OR REPLACE FUNCTION task_bots.choose_startup_email_to_send_bot(task_id int)
+ RETURNS boolean
+ LANGUAGE plpgsql
+AS $function$
+declare
+	sqlstr text;
+	_worker_initials text;
+	_task_id_to_complete int;
+	_parameters json;
+	begin
+		_worker_initials = 'choose_startup_email_to_send_bot'; -- this is for logging		
+		
+		
+		--updating the current task for choose_startup_email_to_send_bot as completed
+		update task_manager.tasks set task_status='completed' where id = task_id;
+		--logging
+		insert into task_bots.logs(botname,"action","result") values (_worker_initials,'Complete task '||cast(task_id as text),'success');		
+		return true;
+	exception when others then
+		insert into task_bots.logs(botname,"action","result") values ('insert_bot','Complete task '||cast(_task_id_to_complete as text) ||' ERROR:'||SQLERRM,'failed');
+		update task_manager.tasks set task_status='completed_failure' where id = task_id;
+		return false;
+	end;
+$function$;
+
+-------------------------------------------------------------------------
+-- Test create task of 'choose_startup_email_to_send' type
+-- select task_bots.create_task ('choose_startup_email_to_send', '{}');

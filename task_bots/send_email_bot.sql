936ccb08aa18f90cd4db0432e1e3754a559ad703
Alexey <aignatyuk@mail.ru>
2017-11-29 19:08:53 +0500

    removed raise notice

--- task_bots/send_email_bot.sql
+++ task_bots/send_email_bot.sql
@@ -41,38 +41,24 @@ declare
 	_email_address text;
 	_ultradox text;
 begin
-		raise notice '-----------------------------------------------STARTING send_email_bot-----------------------------------------------------------------------------------';
 		_worker_initials = 'send_email_bot'; -- this is for logging
 		select task_type_id , parameters  into _task_type_id, _parameters from task_manager.tasks where id = task_id;
 		select parameter_name into _parameter from task_manager.task_parameters where task_type_id=_task_type_id;
 		select query_to_get_variables , view_name, ultradox into _query,_view_name, _ultradox from task_manager.column_task_description where id=_task_type_id;
-		
-		raise notice 'task_type_id = %',_task_type_id;
-		raise notice 'parameters = %',_parameters;
-		raise notice 'query = %',_query;
 		_sqlstr = $$ select  '$$||_parameters||$$'::json ->> '$$|| _parameter||$$'$$;
-		raise notice 'SQL = %',_sqlstr;
 		execute _sqlstr into _param_value;
-		raise notice 'parameter = %',_param_value;
 		_query = replace(_query, '$1', _view_name);
 		_query= replace(_query, '$2', _param_value);
-		raise notice 'query = %',_query;
 		execute _query into _email_address;
-		--select * from task_manager.task_parameters where task_type_id =
 		if  _email_address is null then 
 			raise exception 'Can not find email for %  = %', _parameter ,_param_value;
 		end if;
-		raise notice 'email = %',_email_address;
-		insert into actions (templateurl, action_parameters) values (_ultradox, 
-        hstore('query_that_failed', ' ') || hstore('primary_email', _email_address) || hstore('description', ' '));
+		insert into actions (templateurl, action_parameters) values (_ultradox, hstore('query_that_failed', ' ') || hstore('primary_email', _email_address) || hstore('description', ' '));
 		notify emailer;
 		--updating the current task for send_email_bot as completed
-		raise notice 'task_id = %',task_id;
 		update task_manager.tasks set task_status='completed' where id = task_id;
-		raise notice '-';
 		--logging
 		insert into task_bots.logs(botname,"action","result") values (_worker_initials,'Complete task '||cast(task_id as text),'success');
-		raise notice '-----------------------------------------------send_email_bot END-----------------------------------------------------------------------------------';
 		return true;
 	exception when others then
 		raise notice 'ERROR %',SQLERRM;

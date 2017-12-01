7dba16f9ec35468359d90747cefdb7bd1eedee99
aign <ignatyuk.a@gmail.com>
2017-12-01 14:14:33 +0500

    some modifying

--- task_bots/choose_startup_email_to_send_bot and choose_startup_email_to_send task type.sql
+++ task_bots/choose_startup_email_to_send_bot and choose_startup_email_to_send task type.sql
@@ -11,32 +11,48 @@ insert into task_manager.column_task_description (column_name ,default_freelance
 --insert into  task_manager.task_parameters (task_type_id,parameter_name,data_type) values(1008, 'customer_id','int');
 --insert into  task_manager.task_parameters (task_type_id,parameter_name,data_type) values(1008, 'message_uid','text');
 
-CREATE OR REPLACE FUNCTION task_bots.choose_startup_email_to_send_bot(task_id int)
+CREATE OR REPLACE FUNCTION task_bots.choose_startup_email_to_send_bot(task_id integer)
  RETURNS boolean
  LANGUAGE plpgsql
 AS $function$
 declare
-	sqlstr text;
-	_worker_initials text;
-	_task_id_to_complete int;
+	_sqlstr text;
+	_task_type_id int;
+	_worker_initials text;	
 	_parameters json;
-	begin
-		_worker_initials = 'choose_startup_email_to_send_bot'; -- this is for logging
-		-- select * from task_manager.tasks where id = task_id; -- to get something from current task 		
-		-- select task_bots.create_task ('task_type_name', '{}'); -- to create task of certain task type 
+	_parameter text;
+	_customer_id text;
+	_name text;
+	_function_parameters text[];
+	_success boolean;
+begin
+		_worker_initials = 'choose_startup_email_to_send_bot';
+		select task_type_id , parameters ,name into _task_type_id, _parameters , _name from task_manager.tasks where id = task_id;
+		if _task_type_id is null then
+			select  id into _task_type_id from task_manager.column_task_description where column_name = _name; 
+		end if;
+
+		select parameter_name into _parameter from task_manager.task_parameters where task_type_id=_task_type_id;
+		_sqlstr = $$ select  '$$||_parameters||$$'::json ->> '$$|| _parameter||$$'$$;
+		execute _sqlstr into _customer_id;
+
+		_function_parameters =ARRAY[_customer_id];-- task_bots.create_task function expects text array as 2nd parameter
+		-- also it takes this array as array of values. For example 
+		-- if created task have 2 parameters "customer_id" and "text" 
+		-- then to create task you need to pass only parameter values like ["2","test"] not key/value pairs like [{"customer_id":"2"} , {"text":"test"}] 
+		perform task_bots.create_task ('deleteme_send_random_email_customer'::text, _function_parameters); -- to create task of certain task type 
 		
 		--updating the current task for choose_startup_email_to_send_bot as completed
+		insert into task_bots.logs(botname,"action","result") values (_worker_initials,'Complete task '||cast(task_id as text),'success');
 		update task_manager.tasks set task_status='completed' where id = task_id;
-		--logging
-		insert into task_bots.logs(botname,"action","result") values (_worker_initials,'Complete task '||cast(task_id as text),'success');		
 		return true;
 	exception when others then
-		insert into task_bots.logs(botname,"action","result") values ('insert_bot','Complete task '||cast(_task_id_to_complete as text) ||' ERROR:'||SQLERRM,'failed');
+		raise notice 'ERROR = %',SQLERRM;
+		insert into task_bots.logs(botname,"action","result") values (_worker_initials,'Complete task ERROR:'||SQLERRM,'failed');
 		update task_manager.tasks set task_status='completed_failure' where id = task_id;
 		return false;
 	end;
-$function$;
-
+$function$
 -------------------------------------------------------------------------
 -- Test create task of 'choose_startup_email_to_send' type
 -- select task_bots.create_task ('choose_startup_email_to_send', '{}');

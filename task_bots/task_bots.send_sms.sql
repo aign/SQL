98c6f753a13f3e0faea2a49d125e5668e0bed411
aign <ignatyuk.a@gmail.com>
2018-01-29 19:02:48 +0500

    added task_bots.send_sms

---
+++ task_bots/task_bots.send_sms.sql
@@ -0,0 +1,27 @@
+create or replace function task_bots.send_sms(customer_id int, message_text text)
+returns boolean
+LANGUAGE plpgsql
+AS $function$
+declare
+ _customer_phone text; 
+begin
+	select primary_phone into _customer_phone from public.customers where id = customer_id;
+	insert into emails.actions 
+	(templateurl,action_parameters) values 
+	('https://rest.nexmo.com/sms/json',
+	hstore('from_date',null)|| 
+	hstore('api_key','7ae91a90')||
+	hstore('sendFrom',null)|| 
+	hstore('from','Revisor1')|| 
+	hstore('to',_customer_phone)|| 
+	hstore('text',message_text)|| 
+	hstore('until_date','null')|| 
+	hstore('api_secret','9761fce526ec805a')|| 
+	hstore('mergedHtml.sendFrom',null));
+	notify emailer;
+	return true;
+	exception when others then
+		return false;
+end;
+$function$
+

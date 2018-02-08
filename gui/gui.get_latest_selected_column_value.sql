804e646e834f82ab3d4ab34752439844228e5715
aign <ignatyuk.a@gmail.com>
2018-02-08 17:26:20 +0500

    fixes

--- gui/gui.get_latest_selected_column_value.sql
+++ gui/gui.get_latest_selected_column_value.sql
@@ -7,28 +7,37 @@ declare
   _id int;
   _sqlstr text;
   _result text;
+  _window_name text;
 begin
+	select window_name into _window_name from gui.window_state 
+		where cells_selected->0->>1 = column_name and username = current_setting('session.username') 
+	order by last_updated desc limit 1;
+	raise notice 'un = %',current_setting('session.username');
+	raise notice 'wn = %',_window_name;
 	select TRIM (
  		trailing ';'
  		FROM ((regexp_split_to_array(substring( query, position('from ' in query)+5 ,20),' ') )[1])
  		) into _view_name
- 	from gui.windows where name = (SELECT window_name FROM gui.window_state WHERE username = current_setting('session.username') ORDER BY last_updated DESC LIMIT 1);
+ 	from gui.windows where name = (_window_name);
  	
-	WITH main AS (SELECT * FROM gui.window_state WHERE username = current_setting('session.username') ORDER BY last_updated DESC LIMIT 1),
+	WITH main AS (select * from gui.window_state 
+	where cells_selected->0->>1 = column_name and username = current_setting('session.username') 
+	order by last_updated desc limit 1),
     	 elems AS (SELECT elem FROM main, json_array_elements(main.cells_selected::json) AS elem)
 	SELECT (elem->>0)::int into _id FROM elems WHERE (SELECT COUNT(DISTINCT elem->>0) FROM elems) = 1 and elem->>1 = column_name LIMIT 1;
+	raise notice 'cn = %',column_name;
+	raise notice 'vn = %',_view_name;
+	raise notice 'id = %',_id;
 	_sqlstr = 'select '||column_name||' from '||_view_name||' where id = '||_id;
+	raise notice '%',_sqlstr;
 	execute _sqlstr into _result;
 	return _result;
-exception when others then
+exception when others then 
 	return null;
-	
-end;
+end; 
 $function$
-
 
 --set session.username = 'ignatyuk.a@gmail.com';
 --select gui.get_latest_selected_column_value('primary_phone')
 --select gui.get_latest_selected_column_value('vat_code')
 --select gui.get_latest_selected_column_value('primary_email')
-
--- gui/gui.get_selected_column_value_in_other_window(column_name text).sql
+++ gui/gui.get_selected_column_value_in_other_window(column_name text).sql
@@ -9,26 +9,23 @@ declare
   _result text;
   _window_name text;
 begin
-	select window_name into _window_name from gui.window_state 
-	where cells_selected->0->>1 = 'primary_email' and username = current_setting('session.username') 
-	order by last_updated desc limit 1;
 	select TRIM (
  		trailing ';'
  		FROM ((regexp_split_to_array(substring( query, position('from ' in query)+5 ,20),' ') )[1])
  		) into _view_name
- 	from gui.windows where name = (_window_name);
+ 	from gui.windows where name = (SELECT window_name FROM gui.window_state WHERE username = current_setting('session.username') ORDER BY last_updated DESC LIMIT 1);
  	
-	WITH main AS (select * from gui.window_state 
-	where cells_selected->0->>1 = 'primary_email' and username = current_setting('session.username') 
-	order by last_updated desc limit 1),
+	WITH main AS (SELECT * FROM gui.window_state WHERE username = current_setting('session.username') ORDER BY last_updated DESC LIMIT 1),
     	 elems AS (SELECT elem FROM main, json_array_elements(main.cells_selected::json) AS elem)
 	SELECT (elem->>0)::int into _id FROM elems WHERE (SELECT COUNT(DISTINCT elem->>0) FROM elems) = 1 and elem->>1 = column_name LIMIT 1;
 	_sqlstr = 'select '||column_name||' from '||_view_name||' where id = '||_id;
 	execute _sqlstr into _result;
 	return _result;
-exception when others then 
+exception when others then
 	return null;
+	
 end;
+
 $function$
 
 --set session.username = 'ignatyuk.a@gmail.com'
